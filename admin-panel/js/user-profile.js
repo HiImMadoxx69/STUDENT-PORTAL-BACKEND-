540da6e759d8e1d6a74eae0ec83749585a94b42f
Emmanuel <emmanuel.nocum@cvsu.edu.ph>
2022-05-09 15:45:37 +0800

    Edit Form User Admin

--- admin-panel/js/user-profile.js
+++ admin-panel/js/user-profile.js
@@ -1,118 +1,94 @@
 //Javascript for user-profile 
 const btnUpdateMyProfile = document.getElementById('btnUpdateProfile');//button of update profile info
+
 btnUpdateMyProfile.addEventListener('click', updateProfile);//get the click listener of button update profile
+
 document.getElementById('updateProfileForm').addEventListener('submit', updateProfile);
+
 let btnChangeToLoadingS = document.getElementById('btnChangeToLoading');//loading button
+
 let modalMessage = document.getElementById('modalLogs');// modal message body
-let fileupload = document.getElementById('profileEdit');// fileupload
-const id = document.getElementById('currentUserID').value;
-     //POST NAME
+
+  
+
+//POST NAME
 function updateProfile(e){
- 
-e.preventDefault();
 
+ let currentId = document.getElementById('currentUserID').value;
+    e.preventDefault();
+    let fileupload = document.getElementById('profileEdit');// fileupload
+  
+    let firstname = document.getElementById('firstName').value;
+    let lastname = document.getElementById('lastName').value;
+    let about = document.getElementById('About').value;
+    let position = document.getElementById('Position').value;
+    let address = document.getElementById('Address').value;
+    let contact = document.getElementById('Contact').value;
+    let email = document.getElementById('Email').value;
+    let twitter = document.getElementById('Twitter').value;
+    let facebook = document.getElementById('Facebook').value;
+    let instagram = document.getElementById('Instagram').value;
+    let linkedin = document.getElementById('Linkedin').value; 
+ // Picking up files from the input .  .  .
+ let files = fileupload.files;
 
-let firstname = document.getElementById('firstName').value;
-let lastname = document.getElementById('lastName').value;
-let about = document.getElementById('About').value;
-let position = document.getElementById('Position').value;
-let address = document.getElementById('Address').value;
-let contact = document.getElementById('Contact').value;
-let email = document.getElementById('Email').value;
-let twitter = document.getElementById('Twitter').value;
-let facebook = document.getElementById('Facebook').value;
-let instagram = document.getElementById('Instagram').value;
-let linkedin = document.getElementById('Linkedin').value;
+ // Uploading only one file; multiple uploads are not allowed.
+  let imageFile = files[0]; 
 
+   // Create a FormData object.
+  formData = new FormData();
 
+    // Add the file to the request.
+    formData.append('profileEdit', imageFile, imageFile.name);
+    console.log(currentId);
+    formData.append('userId', currentId);
+    formData.append('firstname', firstname);
+    formData.append('lastname', lastname);
+    formData.append('about', about);
+    formData.append('position', position);
+    formData.append('address', address);
+    formData.append('contact', contact);
+    formData.append('email', email);
+    formData.append('twitter', twitter);
+    formData.append('facebook', facebook);
+    formData.append('instagram', instagram);
+    formData.append('linkedin', linkedin);
+    for (var pair of formData.entries()) {
+      console.log(pair[0]+ ' - ' + pair[1]); 
+   }
+    var xhr = new XMLHttpRequest();
 
-let params =  
-"firstname="+firstname+
-"&lastname="+lastname+
-"&about="+about+
-"&position="+position+
-"&address="+address+
-"&contact="+contact+
-"&email="+email+
-"&twitter="+twitter+
-"&facebook="+facebook+
-"&instagram="+instagram+
-"&linkedin="+linkedin+
-"&id="+id
-;
-
-const xhr = new XMLHttpRequest();
-
-xhr.open('POST', '../controller/user-edit.php', true);
-
-xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-
-
-console.log(params);
-//send
-xhr.send(params);
+    // Open the connection
+    xhr.open('POST', '../controller/user-edit.php', true);
 
 xhr.onprogress = function (){
  
 btnUpdateMyProfile.style.display = "none";
 btnChangeToLoadingS.removeAttribute("hidden");
 };
-
-xhr.onload = function(){
-    let getResult = JSON.parse(this.responseText);
-    setTimeout(delayedFunc, 1000);//Timer for loading
-    function delayedFunc(){
-          if(getResult.statusCode === 200){
-            btnUpdateMyProfile.style.display = "inline-block";
-            btnChangeToLoadingS.setAttribute("hidden", "hidden");
-             $("#basicModal").modal('toggle');//toggle the modal
-             modalMessage.innerText = 'Succesfull';
-             uploadFile();
-          }else{          
-            btnUpdateMyProfile.style.display = "inline-block";
-            btnChangeToLoadingS.setAttribute("hidden", "hidden");
-             $("#basicModal").modal('toggle');//toggle the modal
-             modalMessage.innerText = 'Failed';
-         }//end of if status 200 
-        
-        }//end of delayedFunc
-    
-
-
-  }//onload
-
-
-  function uploadFile() {
-
-  
- // Picking up files from the input .  .  .
- let files = fileupload.files;
-
- // Uploading only one file; multiple uploads are not allowed.
-  let file = files[0]; 
-
-   // Create a FormData object.
-  formData = new FormData();
-
-    // Add the file to the request.
-    formData.append('profileEdit', file, file.name);
-    var xhr = new XMLHttpRequest();
-
-    // Open the connection.
-    xhr.open('POST', '../controller/upload.php', true);
-
-
+  // Send the Data.
+  xhr.send(formData);
     // Set up a handler for when the task for the request is complete.
-    xhr.onload = function () {
-      if (xhr.status === 200) {
-       let getResult = JSON.parse(this.responseText);
-       console.log(getResult.statusCode);
-      } else {
-        console.log("error");
-      }
-    };
+    
+xhr.onload = function(){
+  let getResult = JSON.parse(this.responseText);
+  setTimeout(delayedFunc, 1000);//Timer for loading
+  function delayedFunc(){
+        if(getResult.statusCode === 200){
+          btnUpdateMyProfile.style.display = "inline-block";
+          btnChangeToLoadingS.setAttribute("hidden", "hidden");
+           $("#basicModal").modal('toggle');//toggle the modal
+           modalMessage.innerText = 'Succesfull';
+        }else{          
+          btnUpdateMyProfile.style.display = "inline-block";
+          btnChangeToLoadingS.setAttribute("hidden", "hidden");
+           $("#basicModal").modal('toggle');//toggle the modal
+           modalMessage.innerText = 'Failed';
+       }//end of if status 200 
+      
+      }//end of delayedFunc
+  
 
-    // Send the Data.
-    xhr.send(formData);
-  }
+
+  };//onload
 }
--- admin-panel/pages/user-profile.php
+++ admin-panel/pages/user-profile.php
@@ -762,8 +762,7 @@ echo '<input type = "hidden" id ="currentUserID" value = "'.$user['id'].'"/>';
   <script src="../vendor/quill/quill.min.js"></script>
   <script src="../vendor/simple-datatables/simple-datatables.js"></script>
   <script src="../vendor/tinymce/tinymce.min.js"></script>
-  <script src="../vendor/php-email-form/validate.js"></script>
-
+ 
   <!-- Template Main JS File -->
   <script src="../js/main.js?t=1491313943549"></script>
 
