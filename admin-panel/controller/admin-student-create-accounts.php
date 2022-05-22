b853d417e247c4c1140809b0aad77ed22485d81c
energyrice <104620572+energyrice@users.noreply.github.com>
2022-05-22 23:30:32 +0800

    student create forms

--- admin-panel/controller/admin-student-create-accounts.php
+++ admin-panel/controller/admin-student-create-accounts.php
@@ -12,13 +12,14 @@ $course = $_POST['Course'];
 $section = $_POST['Section'];
 $birthday = $_POST['Birthday'];
 $contact = $_POST['Contact'];
-$gardian = $_POST['GuardianN'];
+$guardian = $_POST['GuardianN'];
 $guardian_contact = $_POST['GuardianCon'];
-if(isset($Fname)){
+
+if(isset($firstname)){
 
     
 try{
-    $sql = "INSERT INTO `tbl_studentinfo` (`profile_url`,`studentnumber`, `password`, `firstname`, `lastname`, `email`, `course`, `section`, `birthday`, `contact`, `guardian`, `guardian_contact`, ) VALUES ('default_profile.jpg', '$studentnumber', '$firstname', '$middlename', '$lastname',  '$email', '$password','$course', '$section', '$contact', '$birthday',  '$guardian', '$guardian_contact');";
+    $sql = "INSERT INTO `tbl_studentinfo` ( `profile_url`, `studentnumber`, `firstname`, `middlename`, `lastname`, `email`, `password`, `course`, `section`, `birthday`, `contact`, `guardian`,`guardian_contact`) VALUES ( 'default_profile.jpg', '$studentnumber', '$firstname', '$middlename', '$lastname', '$email', '$password', '$course', '$section', '$birthday', '$contact', '$guardian', '$guardian_contact');";
     mysqli_query($con, $sql);
     exit(json_encode(array("statusCode"=>200)));
 }catch(Exception $e){
