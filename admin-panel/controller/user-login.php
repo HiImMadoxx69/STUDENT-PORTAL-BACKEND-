eaa20df9b99268442c85032b06471ecbceba35a4
Emmanuel <emmanuel.nocum@cvsu.edu.ph>
2022-07-02 14:53:05 +0800

    added-backend-for-my-react

--- admin-panel/controller/user-login.php
+++ admin-panel/controller/user-login.php
@@ -9,58 +9,54 @@ include_once("../connections/connection.php");
   
 $con = connection();
 
-try{
-  if(isset($_POST['Email'])){
-    $username =  mysqli_real_escape_string($con, $_POST['Email']);
-    $password =   mysqli_real_escape_string($con,$_POST['Password']);
-    
-    $sql = "SELECT * FROM tbl_admin WHERE email = '$username' AND password ='$password' AND status = 'active'";
-    
-    $user = $con ->query($sql) or die ($con->error);
-    $row = $user->fetch_assoc();
-    $total = $user->num_rows;
+
+  if(isset($_POST['username'])){
+  $username =  mysqli_real_escape_string($con, $_POST['username']);
+  $password =   mysqli_real_escape_string($con,$_POST['password']);
   
-    if($total > 0){
-    $_SESSION['ID'] = $row['id'];
-      $_SESSION['Position'] = $row['position'];
-      echo json_encode(array("statusCode"=>200));
-    }else{
-      echo json_encode(array("statusCode"=>201));
-     }
-    }
-    
-    // if(isset($_POST['username'])){
-    //   $username =   $_POST['username'];
-    //   $password =  $_POST['password'];
-      
-    //   $sql = "SELECT * FROM tbl_admin WHERE username = '$username' ";
-      
-    //   $user = $con ->query($sql) or die ($con->error);
-    //   $row = $user->fetch_assoc();
-    //   $total = $user->num_rows;
-    
-    //   if($row > 0){
-        
-    //     if(password_verify($password, $row['password'])) {
-    //       $_SESSION['ID'] = $row['id'];
-    //       $_SESSION['Position'] = $row['position'];
-    //       echo json_encode(array("statusCode"=>200));
-    //     }else{
-    //       $tespass = '1059914Emmanuel';
-    //       $hashME = '$2y$10$4kMy/OZcpil1fZ0pgVtnweYQMz7o0PsglKnIAqF12j3';
-    //       // echo json_encode(array("statusCode"=>$row['password']));
-    //       if(password_verify($tespass, $hashME)){
-    //         exit(json_encode(array("statusCode"=>'valid Hashing')));
-    //     }else{
-    //       exit(json_encode(array("statusCode"=>'Invalid Hashing')));
-    //     }
-    //     }
-    //   }else{
-    //     echo json_encode(array("statusCode"=>201));
-    //     }
-    //   }
-}catch(Exception $e){
-  exit(json_encode(array("statusCode"=>'Invalid fucker')));
-}
+  $sql = "SELECT * FROM tbl_admin WHERE email = '$username' AND password ='$password' AND status = 'active'";
   
+  $user = $con ->query($sql) or die ($con->error);
+  $row = $user->fetch_assoc();
+  $total = $user->num_rows;
+
+  if($total > 0){
+  $_SESSION['ID'] = $row['id'];
+    $_SESSION['Position'] = $row['position'];
+    echo json_encode(array("statusCode"=>200));
+  }else{
+    echo json_encode(array("statusCode"=>201));
+   }
+  }
+  
+  // if(isset($_POST['username'])){
+  //   $username =   $_POST['username'];
+  //   $password =  $_POST['password'];
+    
+  //   $sql = "SELECT * FROM tbl_admin WHERE username = '$username' ";
+    
+  //   $user = $con ->query($sql) or die ($con->error);
+  //   $row = $user->fetch_assoc();
+  //   $total = $user->num_rows;
+  
+  //   if($row > 0){
+      
+  //     if(password_verify($password, $row['password'])) {
+  //       $_SESSION['ID'] = $row['id'];
+  //       $_SESSION['Position'] = $row['position'];
+  //       echo json_encode(array("statusCode"=>200));
+  //     }else{
+  //       $tespass = '1059914Emmanuel';
+  //       $hashME = '$2y$10$4kMy/OZcpil1fZ0pgVtnweYQMz7o0PsglKnIAqF12j3';
+  //       // echo json_encode(array("statusCode"=>$row['password']));
+  //       if(password_verify($tespass, $hashME)){
+  //         exit(json_encode(array("statusCode"=>'valid Hashing')));
+  //     }else{
+  //       exit(json_encode(array("statusCode"=>'Invalid Hashing')));
+  //     }
+  //     }
+  //   }else{
+  //     echo json_encode(array("statusCode"=>201));
+  //     }
+  //   }
 ?>
