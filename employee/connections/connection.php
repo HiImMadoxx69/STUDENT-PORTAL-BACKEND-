6fabe1a7b4d68f8a71fe7754ea2e0298724bcc69
Emmanuel <emmanuel.nocum@cvsu.edu.ph>
2022-07-05 16:01:48 +0800

    remove id and password to api

--- employee/connections/connection.php
+++ employee/connections/connection.php
@@ -1,22 +1,38 @@
 <?php
 
 function connection(){
-    //Get Heroku ClearDB connection information
-    $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-    $cleardb_server = $cleardb_url["host"];
-    $cleardb_username = $cleardb_url["user"];
-    $cleardb_password = $cleardb_url["pass"];
-    $cleardb_db = substr($cleardb_url["path"],1);
-    $active_group = 'default';
-    $query_builder = TRUE;
-    // Connect to DB
-    $con = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+ //Get Heroku ClearDB connection information
+ $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+ $cleardb_server = $cleardb_url["host"];
+ $cleardb_username = $cleardb_url["user"];
+ $cleardb_password = $cleardb_url["pass"];
+ $cleardb_db = substr($cleardb_url["path"],1);
+ $active_group = 'default';
+ $query_builder = TRUE;
+ // Connect to DB
+ $con = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
+ if($con->connect_error){
+     echo $con->connect_error;
+ }else{
+     return $con;
+ }
+
  
-    if($con->connect_error){
-        echo $con->connect_error;
-    }else{
-        return $con;
-    }
-}
+ //localhost
+//  $host = "localhost";
+//  $username = "root";
+//  $password = "";
+//  $database = "db_admin";
+
+
+//  $con = new mysqli($host, $username, $password, $database,);
+ 
+//  if($con->connect_error){
+//      echo $con->connect_error;
+//  }else{
+//      return $con;
+//  }
+// }
 
 ?>
