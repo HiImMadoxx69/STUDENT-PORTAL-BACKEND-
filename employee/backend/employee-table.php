764452c64460f097828e655c64644b929cbfc2ed
Emmanuel <emmanuel.nocum@cvsu.edu.ph>
2022-09-07 21:02:19 +0800

    Access control

---
+++ employee/backend/employee-table.php
@@ -0,0 +1,29 @@
+<?php
+
+try{
+    header('Access-Control-Allow-Origin: *');
+    header('Content-type: application/json');
+    include_once("../connections/connection.php");
+$con = connection();
+
+
+$sql = mysqli_query($con, "SELECT 
+id, profile_url, email,username,firstname,middlename,lastname,birthday
+,sex,position,address,contact,about
+,twitterprofile
+,facebookprofile
+,instagramprofile
+,linkedinprofile
+,status
+,added_at FROM `tbl_admin` ORDER BY `added_at` DESC");
+
+//store in result
+
+$result = mysqli_fetch_all($sql, MYSQLI_ASSOC);
+
+exit(json_encode($result));
+}catch (Exception $e){
+
+}
+
+?>
