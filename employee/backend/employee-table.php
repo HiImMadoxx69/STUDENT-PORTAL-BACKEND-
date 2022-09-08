47e401ee99a9b3f81d7f8f8edc064cd108b136d1
Emmanuel <emmanuel.nocum@cvsu.edu.ph>
2022-09-08 10:14:05 +0800

    Putangina ang init

--- employee/backend/employee-table.php
+++ employee/backend/employee-table.php
@@ -1,8 +1,7 @@
 <?php
- header('Access-Control-Allow-Origin: *');
- header('Content-type: application/json');
 try{
-   
+    header('Access-Control-Allow-Origin: *');
+    header('Content-type: application/json');
     include_once("../connections/connection.php");
 $con = connection();
 
@@ -23,7 +22,7 @@ $result = mysqli_fetch_all($sql, MYSQLI_ASSOC);
 
 exit(json_encode($result));
 }catch (Exception $e){
-
+    exit(json_encode(array("statusCode"=>$e->getMessage())));
 }
 
 ?>
