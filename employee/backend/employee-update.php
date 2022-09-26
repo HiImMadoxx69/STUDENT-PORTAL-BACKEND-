2d13ff5f9f71b47aca25ff07b948e35866ffa039
Emmanuel <emmanuel.nocum@cvsu.edu.ph>
2022-09-26 20:42:02 +0800

    asd

---
+++ employee/backend/employee-update.php
@@ -0,0 +1,56 @@
+<?php
+ header('Access-Control-Allow-Origin: *');
+ header('Content-type: application/json');
+include_once("../connections/connection.php");
+$con = connection();
+
+
+// $uploadDirectory = "../../uploads/";
+
+$errors = []; // Store all foreseen and unforeseen errors here.
+
+// $fileExtensions = ['jpeg','jpg','png']; // Get all the file extensions.
+
+// $fileName = $_FILES['profileEdit']['name'];
+// $fileSize = $_FILES['profileEdit']['size'];
+// $fileTmpName  = $_FILES['profileEdit']['tmp_name'];
+// $fileType = $_FILES['profileEdit']['type'];
+// $fileExtension = strtolower(end(explode('.',$fileName)));
+
+// $uploadPath = $uploadDirectory . basename($fileName); 
+
+// echo $uploadPath;
+
+  $userCurrentId = $_POST['UserId'];  
+  $fname = $_POST['Fisrtname'];
+  $mname = $_POST['Middlename'];
+  $lname = $_POST['Lastname'];
+  $birthday = $_POST['Birthday'];
+  // exit(json_encode(array("statusCode"=>$birthday)));
+  $sex = $_POST['Sex'];
+  $about = $_POST['About'];
+  $position = $_POST['Job'];
+  $address = $_POST['Address'];
+  $contact = $_POST['Contact'];
+  $twitter = $_POST['Twitter'];
+  $facebook = $_POST['Facebook'];
+  $instagram = $_POST['Instagram'];
+  $linkedin = $_POST['Linkedin'];
+
+  
+if (isset($userCurrentId)) {
+ 
+ 
+ try{
+                  $sql = "UPDATE `tbl_admin` SET `firstname` = '$fname', `middlename` = '$mname', `lastname` = '$lname', `birthday` = '$birthday', `sex` = '$sex', `position` = '$position', `address` = '$address', `contact` = '$contact', `about` = '$about', `twitterprofile` = '$twitter', `facebookprofile` = '$facebook', `instagramprofile` = '$instagram', `linkedinprofile` = '$linkedin' WHERE `tbl_admin`.`id` = $userCurrentId;";
+    
+                  mysqli_query($con, $sql);
+
+
+                  $auditsql = "INSERT INTO `tbl_audit` (`action`) VALUES ('Updated: User Account rowID: $userCurrentId');";
+                  mysqli_query($con, $auditsql);
+                  exit(json_encode(array("statusCode"=>200)));
+ }catch(Exception $e){
+  exit(json_encode(array("statusCode"=>$e->getMessage())));
+ }
+}
