bfcd78b71afdc232f7c35434b0d41535bd129dbf
Emmanuel <emmanuel.nocum@cvsu.edu.ph>
2022-09-27 23:30:50 +0800

    wasf

---
+++ employee/backend/employee-history.php
@@ -0,0 +1,19 @@
+<?php
+ header('Access-Control-Allow-Origin: *');
+ header('Content-type: application/json');
+include_once("../connections/connection.php");
+$con = connection();
+
+try{
+    
+
+    $sql = "SELECT * from `tbl_admin` WHERE `category` =  'Employee'";
+    mysqli_query($con, $sql);
+    $user = $con ->query($sql) or die ($con->error);
+    $row = $user->fetch_assoc();
+    exit(json_encode(array("statusCode"=>$row)));
+}catch(Exception $e){
+
+}
+
+?>
