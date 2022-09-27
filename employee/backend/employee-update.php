47623148d12c473af5fb29c90b047babc54a60a1
Emmanuel <emmanuel.nocum@cvsu.edu.ph>
2022-09-27 22:02:22 +0800

    bcvb

--- employee/backend/employee-update.php
+++ employee/backend/employee-update.php
@@ -48,27 +48,27 @@ if (isset($email)) {
  
  
  try{
-                // $beforeSql = "SELECT `firstname`, `middlename`, `lastname`, `birthday`, `sex`, `position`,`address`, `contact`, `about`, `twitterprofile`, `facebookprofile`, `instagramprofile`,`linkedinprofile` WHERE `tbl_admin`.`email` = '$email';";     
+                $beforeSql = "SELECT `firstname`, `middlename`, `lastname`, `birthday`, `sex`, `position`,`address`, `contact`, `about`, `twitterprofile`, `facebookprofile`, `instagramprofile`,`linkedinprofile` WHERE `tbl_admin`.`email` = '$email';";     
                 
-                // mysqli_query($con, $beforeSql);
+                mysqli_query($con, $beforeSql);
 
-                // $getBefore = $con ->query($beforeSql) or die ($con->error);
-                // $rowBefore = $getBefore ->fetch_assoc();
+                $getBefore = $con ->query($beforeSql) or die ($con->error);
+                $rowBefore = $getBefore ->fetch_assoc();
 
                   $sql = "UPDATE `tbl_admin` SET `firstname` = '$fname', `middlename` = '$mname', `lastname` = '$lname', `birthday` = '$birthday', `sex` = '$sex', `position` = '$position', `address` = '$address', `contact` = '$contact', `about` = '$about', `twitterprofile` = '$twitter', `facebookprofile` = '$facebook', `instagramprofile` = '$instagram', `linkedinprofile` = '$linkedin' WHERE `tbl_admin`.`email` = '$email';";
     
                   mysqli_query($con, $sql);
 
-                //   $AfterSql = "SELECT `firstname`, `middlename`, `lastname`, `birthday`, `sex`, `position`,`address`, `contact`, `about`, `twitterprofile`, `facebookprofile`, `instagramprofile`,`linkedinprofile` WHERE `tbl_admin`.`email` = '$email';";     
+                  $AfterSql = "SELECT `firstname`, `middlename`, `lastname`, `birthday`, `sex`, `position`,`address`, `contact`, `about`, `twitterprofile`, `facebookprofile`, `instagramprofile`,`linkedinprofile` WHERE `tbl_admin`.`email` = '$email';";     
                 
-                // mysqli_query($con, $AfterSql);
+                mysqli_query($con, $AfterSql);
 
-                // $getAfter = $con ->query($AfterSql) or die ($con->error);
-                // $rowAfter = $getAfter ->fetch_assoc();
+                $getAfter = $con ->query($AfterSql) or die ($con->error);
+                $rowAfter = $getAfter ->fetch_assoc();
 
 
-                  // $auditsql = "INSERT INTO `tbl_updatehistory` (`action`,`category`,`editor_position`,`editor_email`,`edited_email`,`before_edit`,`after_edit`) VALUES ('$action','$category','$editPosition','$editEmail', '$email', '$rowBefore', '$rowAfter' );";
-                  // mysqli_query($con, $auditsql);
+                  $auditsql = "INSERT INTO `tbl_updatehistory` (`action`,`category`,`editor_position`,`editor_email`,`edited_email`,`before_edit`,`after_edit`) VALUES ('$action','$category','$editPosition','$editEmail', '$email', '$rowBefore', '$rowAfter' );";
+                  mysqli_query($con, $auditsql);
 
                   $xsql = "SELECT * from `tbl_admin` WHERE `email` =  '$email'";
         mysqli_query($con, $xsql);
