b3026ad2028894bddd3dec8f52d6469aa294ae4c
Emmanuel <emmanuel.nocum@cvsu.edu.ph>
2022-09-30 18:08:19 +0800

    asd

--- employee/backend/employee-add.php
+++ employee/backend/employee-add.php
@@ -1,98 +1,98 @@
 <?php
 try{
-
+    header('Access-Control-Allow-Origin: *');
+    header('Content-type: application/json');
+   //includes/PHPMailer.php
+   //include required phpmailerfiles
+   require_once '../phpmailer/includes/PHPMailer.php';
+   require_once '../phpmailer/includes/SMTP.php';
+   require_once '../phpmailer/includes/Exception.php';
+   //Define name spaces
+   use PHPMailer\PHPMailer\PHPMailer;
+   use PHPMailer\PHPMailer\SMTP;
+   use PHPMailer\PHPMailer\Exception;
+   
+   
+       include_once("../connections/connection.php");
+   $con = connection();
+   
+   
+   $Fname = $_POST['Fname'];
+   $Mname = $_POST['Mname'];
+   $Lname = $_POST['Lname'];
+   $Email = $_POST['Email'];
+   $Password = $_POST['Password'];
+   $Position = $_POST['Position'];
+   $Birthday = $_POST['Birthday'];
+   $Sex = $_POST['Sex'];
+   $Contact = $_POST['Contact'];
+   $Address = $_POST['Address'];
+   $Twitter = $_POST['Twitter'];
+   $Facebook = $_POST['Facebook'];
+   $Instagram = $_POST['Instagram'];
+   $Linkedin = $_POST['Linkedin'];
+   
+   try{
+       
+   $check = false;
+   
+   // $hashed_password = password_hash($Password, PASSWORD_DEFAULT);
+   //Create instance of phpmailer
+   $mail = new PHPMailer();
+   //Set mailer to use smtp
+   $mail->isSMTP();
+   //define smtp host
+   $mail->Host = "smtp.gmail.com";
+   //enable smtp authentication
+   $mail->SMTPAuth = "true";
+   //set type of encryption (ssl/tls)
+   $mail->SMTPSecure = "tls";
+   //set port to connect smtp
+   $mail->Port = '587';
+   //set gmail username
+   $mail->Username = 'nocumadoxx@gmail.com';
+   //set gmail password
+   $mail->Password = 'tjmgrybklskwxtia';
+   //set email subject
+   $mail->Subject = 'Test Email Using PHPMailer';
+   //set sender email
+   $mail->setFrom('nocumadoxx@gmail.com');
+   //Enable HTML
+   $mail->isHTML(true);
+   //Email body
+   $mail->Body ="<h1>HI! Our School Now Have A New $Position! </h1> <h2>Welcome to our Family!</h2>
+   <p>You can now login to our Employee portal: Your Password = $Password</p>";
+   //Add recipient
+   $mail->addAddress($Email);
+   //Finally send email
+   try {
+       $mail->Send();
+       //Closing smtp connection
+   $mail->smtpClose();
+       $check = true;
+   } catch (Exception $e) {
+       exit(json_encode(array("statusCode"=>201)));
+   }
+   
+   
+       $sql = "INSERT INTO `tbl_admin` (`profile_url`, `email`,`password`, `firstname`, `middlename`, `lastname`, `birthday`, `sex`, `position`, `address`, `contact`, `twitterprofile`, `facebookprofile`, `instagramprofile`, `linkedinprofile`) VALUES ('default_profile.jpg', '$Email', '$Username','$Password', '$Fname', '$Mname', '$Lname', '$Birthday', '$Sex', '$Position', '$Address', '$Contact', '$Twitter', '$Facebook', '$Instagram', '$Linkedin');";
+       mysqli_query($con, $sql);
+   
+       $auditsql = "INSERT INTO `tbl_audit` (`action`) VALUES ('Created a new User Account');";
+       mysqli_query($con, $auditsql);
+       exit(json_encode(array("statusCode"=>200)));
+   
+     
+   }catch(Exception $e){
+       exit(json_encode(array("statusCode"=>$e->getMessage())));
+   }
+   
+   // $getFiles = $_POST['floatingFname'];
+   // if(isset($getFiles)){
+   //     exit(json_encode(array("statusCode"=>201)));
+   // }
+   
 }
- header('Access-Control-Allow-Origin: *');
- header('Content-type: application/json');
-//includes/PHPMailer.php
-//include required phpmailerfiles
-require_once '../phpmailer/includes/PHPMailer.php';
-require_once '../phpmailer/includes/SMTP.php';
-require_once '../phpmailer/includes/Exception.php';
-//Define name spaces
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
-
-
-    include_once("../connections/connection.php");
-$con = connection();
-
-
-$Fname = $_POST['Fname'];
-$Mname = $_POST['Mname'];
-$Lname = $_POST['Lname'];
-$Email = $_POST['Email'];
-$Password = $_POST['Password'];
-$Position = $_POST['Position'];
-$Birthday = $_POST['Birthday'];
-$Sex = $_POST['Sex'];
-$Contact = $_POST['Contact'];
-$Address = $_POST['Address'];
-$Twitter = $_POST['Twitter'];
-$Facebook = $_POST['Facebook'];
-$Instagram = $_POST['Instagram'];
-$Linkedin = $_POST['Linkedin'];
-
-try{
-    
-$check = false;
-
-// $hashed_password = password_hash($Password, PASSWORD_DEFAULT);
-//Create instance of phpmailer
-$mail = new PHPMailer();
-//Set mailer to use smtp
-$mail->isSMTP();
-//define smtp host
-$mail->Host = "smtp.gmail.com";
-//enable smtp authentication
-$mail->SMTPAuth = "true";
-//set type of encryption (ssl/tls)
-$mail->SMTPSecure = "tls";
-//set port to connect smtp
-$mail->Port = '587';
-//set gmail username
-$mail->Username = 'nocumadoxx@gmail.com';
-//set gmail password
-$mail->Password = 'tjmgrybklskwxtia';
-//set email subject
-$mail->Subject = 'Test Email Using PHPMailer';
-//set sender email
-$mail->setFrom('nocumadoxx@gmail.com');
-//Enable HTML
-$mail->isHTML(true);
-//Email body
-$mail->Body ="<h1>HI! Our School Now Have A New $Position! </h1> <h2>Welcome to our Family!</h2>
-<p>You can now login to our Employee portal: Your Password = $Password</p>";
-//Add recipient
-$mail->addAddress($Email);
-//Finally send email
-try {
-    $mail->Send();
-    //Closing smtp connection
-$mail->smtpClose();
-    $check = true;
-} catch (Exception $e) {
-    exit(json_encode(array("statusCode"=>201)));
-}
-
-
-    $sql = "INSERT INTO `tbl_admin` (`profile_url`, `email`,`password`, `firstname`, `middlename`, `lastname`, `birthday`, `sex`, `position`, `address`, `contact`, `twitterprofile`, `facebookprofile`, `instagramprofile`, `linkedinprofile`) VALUES ('default_profile.jpg', '$Email', '$Username','$Password', '$Fname', '$Mname', '$Lname', '$Birthday', '$Sex', '$Position', '$Address', '$Contact', '$Twitter', '$Facebook', '$Instagram', '$Linkedin');";
-    mysqli_query($con, $sql);
-
-    $auditsql = "INSERT INTO `tbl_audit` (`action`) VALUES ('Created a new User Account');";
-    mysqli_query($con, $auditsql);
-    exit(json_encode(array("statusCode"=>200)));
-
-  
-}catch(Exception $e){
-    exit(json_encode(array("statusCode"=>$e->getMessage())));
-}
-
-// $getFiles = $_POST['floatingFname'];
-// if(isset($getFiles)){
-//     exit(json_encode(array("statusCode"=>201)));
-// }
 
 catch(Exception $e){
     exit(json_encode(array("statusCode"=>$e->getMessage())));
