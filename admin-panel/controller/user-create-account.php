fc60a3b3440819eb6fb75351bd37465264db134a
Emmanuel <emmanuel.nocum@cvsu.edu.ph>
2022-09-30 21:26:12 +0800

    xcvxcv

--- admin-panel/controller/user-create-account.php
+++ admin-panel/controller/user-create-account.php
@@ -95,7 +95,7 @@ $mail->smtpClose();
 
     
 
-    $auditsql = "INSERT INTO `tbl_updatehistory` (`action`,`category`,`editor_position`,`editor_email`,`edited_email`,`before_edit`) VALUES ('$action','$category','$editPosition','$editEmail', '$email', '$rowBefore' );";
+    $auditsql = "INSERT INTO `tbl_updatehistory` (`action`,`category`,`editor_position`,`editor_email`,`edited_email`,`before_edit`) VALUES ('$action','$category','$editPosition','$editEmail', '$Email', '$rowBefore' );";
     mysqli_query($con, $auditsql);
 
     exit(json_encode(array("statusCode"=>200)));
