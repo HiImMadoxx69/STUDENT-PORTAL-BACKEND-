9a64191ad239a889b71697432d7941ae035d0ae6
Emmanuel <emmanuel.nocum@cvsu.edu.ph>
2022-10-06 22:06:18 +0800

    cvbcvb

---
+++ employee/backend/course-add.php
@@ -0,0 +1,47 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Content-type: application/json');
+
+include_once("../connections/connection.php");
+$con = connection();
+
+
+$Faculty = $_POST['Faculty'];
+$Course = $_POST['Course'];
+$Description = $_POST['Description'];
+
+$editPosition = $_POST['EditorPosition'];
+$editEmail = $_POST['EditorEmail'];
+$category =  $_POST['Category'];
+$action = $_POST['Action'];
+
+try{
+    
+
+
+    $sql = "INSERT INTO `tbl_course` (`course_faculty`,`course_name`,`description`) VALUES ('$Faculty','$Course','$Description');";
+    mysqli_query($con, $sql);
+
+
+    $BeforeSql = "SELECT * FROM tbl_course WHERE course_name = '$Course'";     
+                
+    mysqli_query($con, $BeforeSql);
+
+    $getBefore = $con ->query($BeforeSql) or die ($con->error);
+    $rowBefore = json_encode($getBefore ->fetch_assoc());
+
+
+    
+
+    $auditsql = "INSERT INTO `tbl_updatehistory` (`action`,`category`,`editor_position`,`editor_email`,`edited_email`,`before_edit`) VALUES ('$action','$category','$editPosition','$editEmail', '$Course', '$rowBefore' );";
+    mysqli_query($con, $auditsql);
+
+    exit(json_encode(array("statusCode"=>200)));
+
+  
+}catch(Exception $e){
+    exit(json_encode(array("statusCode"=>$e->getMessage())));
+}
+
+
+?>
