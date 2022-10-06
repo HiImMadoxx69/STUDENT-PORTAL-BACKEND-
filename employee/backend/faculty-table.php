7158fb139d1f5019ced34f1b040ee556e0aaae2c
Emmanuel <emmanuel.nocum@cvsu.edu.ph>
2022-10-06 14:02:44 +0800

    xcvs

---
+++ employee/backend/faculty-table.php
@@ -0,0 +1,17 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Content-type: application/json');
+
+include_once("../connections/connection.php");
+$con = connection();
+
+
+$sql = mysqli_query($con, "SELECT * FROM `tbl_faculty`  ORDER BY `id` DESC");
+
+//store in result
+
+$result = mysqli_fetch_all($sql, MYSQLI_ASSOC);
+
+exit(json_encode($result));
+
+?>
