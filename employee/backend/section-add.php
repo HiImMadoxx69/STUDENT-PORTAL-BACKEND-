900046590154a32404b81a967ddc95be1c217a5d
Emmanuel <emmanuel.nocum@cvsu.edu.ph>
2022-10-20 23:10:05 +0800

    xcvxc

--- employee/backend/section-add.php
+++ employee/backend/section-add.php
@@ -24,7 +24,7 @@ try{
     mysqli_query($con, $existSQL);
 
     $toInt = (int) $existSQL;
-    if($toInt > 1){
+    if($existSQL > 1){
         $SectionName  = null;
         $uniqueSection = $toInt + 1;
         $SectionName = $Course.' '.substr($Year,0,1).'-'.$toInt;
