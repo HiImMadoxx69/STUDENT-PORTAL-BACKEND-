d92d6b8cf8e3f3a4e10aa2ba695c857db023e2b7
Emmanuel <emmanuel.nocum@cvsu.edu.ph>
2022-10-21 13:59:35 +0800

    xcvxcvxcvxcv

--- employee/backend/section-name-generate.php
+++ employee/backend/section-name-generate.php
@@ -15,11 +15,10 @@ $AcademicYear = $_POST['AcademicYear'];
 try{
 
     $existSQL = "SELECT COUNT(*) FROM tbl_section WHERE course = '$Course' AND section_year = '$Year' AND academic_year = '$AcademicYear'";
-    $dataAdd = mysqli_query($con, $existSQL);
-
-    exit(json_encode(array("statusCode"=>$dataAdd)));
-
-  
+    if($dataAdd = mysqli_query($con, $existSQL)){
+        $rowcount = mysqli_num_rows($dataAdd);
+        exit(json_encode(array("statusCode"=>$rowcount)));
+    }  
 }catch(Exception $e){
     exit(json_encode(array("statusCode"=>$e->getMessage())));
 }
