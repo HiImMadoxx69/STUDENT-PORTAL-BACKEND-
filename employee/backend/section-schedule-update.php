073025a95b21b7bd7be29daedf0be2a8ccdc9d6f
Emmanuel <emmanuel.nocum@cvsu.edu.ph>
2022-10-27 14:16:45 +0800

    xcvxcv

---
+++ employee/backend/section-schedule-update.php
@@ -0,0 +1,61 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Content-type: application/json');
+include_once("../connections/connection.php");
+$con = connection();
+
+try{
+
+
+
+$CurrentId  = $_POST['ID'];
+$ProfessorUsername = $_POST['ProfessorUsername'];
+$Time = $_POST['Time'];
+$Day = $_POST['Day'];
+$Status = $_POST['Status'];
+$Action = $_POST['Action'];
+$EditorPosition = $_POST['EditorPosition'];
+$EditorEmail = $_POST['EditorEmail'];
+$Category = $_POST['Category'];
+//sd
+try{
+    
+    if (isset($CurrentId)) {
+
+        $beforeSql = "SELECT * FROM tbl_subjectpersection WHERE `id` = '$CurrentId';";     
+       
+        mysqli_query($con, $beforeSql);
+
+        $getBefore = $con ->query($beforeSql) or die ($con->error);
+        $setBefore =  $getBefore ->fetch_assoc();
+        $rowBefore = json_encode($setBefore);
+
+        $sql = "UPDATE `tbl_subjectpersection` SET `professor_initial` = '$ProfessorUsername',`schedule_time` = '$Time',`schedule_day` = '$Day',`status` = '$Status' WHERE `tbl_subjectpersection`.`id` = $CurrentId;";
+        mysqli_query($con, $sql);
+       
+        $AfterSql = "SELECT * FROM tbl_subjectpersection WHERE `id` = $CurrentId;";     
+                
+        mysqli_query($con, $AfterSql);
+
+        $getAfter = $con ->query($AfterSql) or die ($con->error);
+        $rowAfter = json_encode($getAfter ->fetch_assoc());
+        
+
+        $auditsql = "INSERT INTO `tbl_updatehistory` (`action`,`category`,`editor_position`,`editor_email`,`edited_email`,`after_edit`,`before_edit`) VALUES ('$Action','$Category','$EditorPosition','$EditorEmail', '$CurrentId', '$rowAfter','$rowBefore');";
+                  mysqli_query($con, $auditsql);
+
+                  $xsql = "SELECT * from `tbl_subjectpersection` WHERE `id` = '$CurrentId'";
+        mysqli_query($con, $xsql);
+
+        $user = $con ->query($xsql) or die ($con->error);
+        $row = $user->fetch_assoc();
+        exit(json_encode(array("statusCode"=>$row)));
+    }
+}catch(Exception $e){
+    exit(json_encode(array("statusCode"=>$e->getMessage())));
+}
+
+}catch(Exception $e){
+    exit(json_encode(array("statusCode"=>$e->getMessage())));
+}
+?>
