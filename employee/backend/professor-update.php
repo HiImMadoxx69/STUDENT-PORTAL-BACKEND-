1ed6ac86d70a4a770265a59a6e031f2b7c1621ba
Emmanuel <emmanuel.nocum@cvsu.edu.ph>
2022-10-28 12:39:43 +0800

    cvbcvbcvbcv

---
+++ employee/backend/professor-update.php
@@ -0,0 +1,61 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Content-type: application/json');
+include_once("../connections/connection.php");
+$con = connection();
+
+try{
+
+$FirstName = $_POST['FirstName'];
+$MiddleName = $_POST['MiddleName'];
+$LastName = $_POST['LastName'];
+$Email = $_POST['Email'];
+$Username = $_POST['Username'];
+$Faculty = $_POST['Faculty'];
+$Status = $_POST['Status'];
+$Action = $_POST['Action'];
+$EditorPosition = $_POST['EditorPosition'];
+$EditorEmail = $_POST['EditorEmail'];
+$Category = $_POST['Category'];
+
+try{
+    
+    if (isset($Username)) {
+
+        $beforeSql = "SELECT * FROM tbl_professor WHERE `professor_username` = '$Username';";     
+       
+        mysqli_query($con, $beforeSql);
+
+        $getBefore = $con ->query($beforeSql) or die ($con->error);
+        $setBefore =  $getBefore ->fetch_assoc();
+        $rowBefore = json_encode($setBefore);
+
+        $sql = "UPDATE `tbl_professor` SET `firstname` = '$FirstName', `middlename` = '$MiddleName',`lastname` = '$LastName',`faculty` = '$Faculty',` `status` = '$Status' WHERE `tbl_professor`.`id` = '$Username';";
+        mysqli_query($con, $sql);
+       
+        $AfterSql = "SELECT * FROM tbl_professor WHERE `id` = '$Username';";     
+                
+        mysqli_query($con, $AfterSql);
+
+        $getAfter = $con ->query($AfterSql) or die ($con->error);
+        $rowAfter = json_encode($getAfter ->fetch_assoc());
+        
+
+        $auditsql = "INSERT INTO `tbl_updatehistory` (`action`,`category`,`editor_position`,`editor_email`,`edited_email`,`after_edit`,`before_edit`) VALUES ('$Action','$Category','$EditorPosition','$EditorEmail', '$Username', '$rowAfter','$rowBefore');";
+                  mysqli_query($con, $auditsql);
+
+                  $xsql = "SELECT * from `tbl_professor` WHERE `professor_username` =  '$Username'";
+        mysqli_query($con, $xsql);
+
+        $user = $con ->query($xsql) or die ($con->error);
+        $row = $user->fetch_assoc();
+        exit(json_encode(array("statusCode"=>$row)));
+    }
+}catch(Exception $e){
+    exit(json_encode(array("statusCode"=>$e->getMessage())));
+}
+
+}catch(Exception $e){
+    exit(json_encode(array("statusCode"=>$e->getMessage())));
+}
+?>
