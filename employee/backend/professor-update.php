254614309a72470ec757460dff6a692c01913c53
Emmanuel <emmanuel.nocum@cvsu.edu.ph>
2022-10-28 13:00:40 +0800

    dfbdfbd

--- employee/backend/professor-update.php
+++ employee/backend/professor-update.php
@@ -33,7 +33,7 @@ try{
         $sql = "UPDATE `tbl_professor` SET `firstname` = '$FirstName', `middlename` = '$MiddleName',`lastname` = '$LastName',`faculty` = '$Faculty', `status` = '$Status' WHERE `professor_username` = '$Username';";
         mysqli_query($con, $sql);
        
-        $AfterSql = "SELECT * FROM tbl_professor WHERE `id` = '$Username';";     
+        $AfterSql = "SELECT * FROM tbl_professor WHERE `professor_username` = '$Username';";     
                 
         mysqli_query($con, $AfterSql);
 
@@ -44,7 +44,7 @@ try{
         $auditsql = "INSERT INTO `tbl_updatehistory` (`action`,`category`,`editor_position`,`editor_email`,`edited_email`,`after_edit`,`before_edit`) VALUES ('$Action','$Category','$EditorPosition','$EditorEmail', '$Username', '$rowAfter','$rowBefore');";
                   mysqli_query($con, $auditsql);
 
-                  $xsql = "SELECT * from `tbl_professor` WHERE `professor_username` =  '$Username'";
+                  $xsql = "SELECT * from `tbl_professor` WHERE `professor_username` =  '$Username';";
         mysqli_query($con, $xsql);
 
         $user = $con ->query($xsql) or die ($con->error);
