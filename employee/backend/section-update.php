5d8785cf508d6bf577458ae709394dda7ba5c485
Emmanuel <emmanuel.nocum@cvsu.edu.ph>
2022-10-31 16:26:04 +0800

    asdsad

--- employee/backend/section-update.php
+++ employee/backend/section-update.php
@@ -40,7 +40,7 @@ try{
         $rowAfter = json_encode($getAfter ->fetch_assoc());
         
 
-        $auditsql = "INSERT INTO `tbl_updatehistory` (`action`,`category`,`editor_position`,`editor_email`,`edited_email`,`after_edit`,`before_edit`) VALUES ('$Action','$Category','$EditorPosition','$EditorEmail', '$SectionName', '$rowAfter','$rowBefore');";
+        $auditsql = "INSERT INTO `tbl_updatehistory` (`action`,`category`,`editor_position`,`editor_email`,`edited_email`,`after_edit`,`before_edit`) VALUES ('$Action','$Category','$EditorPosition','$EditorEmail', '$CurrentId', '$rowAfter','$rowBefore');";
                   mysqli_query($con, $auditsql);
 
                   $xsql = "SELECT * from `tbl_section` WHERE `id` = '$CurrentId'";
