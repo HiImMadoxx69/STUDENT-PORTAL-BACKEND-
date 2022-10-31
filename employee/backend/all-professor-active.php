ed889e8571a660a0bf8486c422b3ef2514fbe70f
Emmanuel <emmanuel.nocum@cvsu.edu.ph>
2022-10-31 18:35:32 +0800

    xcvxcv

---
+++ employee/backend/all-professor-active.php
@@ -0,0 +1,16 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Content-type: application/json');
+include_once("../connections/connection.php");
+$con = connection();
+
+
+$sql = mysqli_query($con, "SELECT 
+id FROM `tbl_professor` WHERE status = 'active' ORDER BY `added_at` DESC");
+
+//store in result
+
+$result = mysqli_fetch_all($sql, MYSQLI_ASSOC);
+
+exit(json_encode($result));
+?>
