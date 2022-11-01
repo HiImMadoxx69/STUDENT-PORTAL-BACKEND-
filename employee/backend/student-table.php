342ffa3c4673b0e128ac115a53a694fcc54784f5
Emmanuel <emmanuel.nocum@cvsu.edu.ph>
2022-11-01 14:22:59 +0800

    cvbcvb

--- employee/backend/student-table.php
+++ employee/backend/student-table.php
@@ -8,7 +8,7 @@ $con = connection();
 
 $sql = mysqli_query($con, "SELECT 
 id, profile_url, studentnumber, firstname, middlename,lastname, email,address,sex,course,section, birthday,contact,guardian,guardian_contact,balance,status
-,added_at FROM `tbl_admin` ORDER BY `added_at` DESC");
+,added_at FROM `tbl_studentinfo` ORDER BY `added_at` DESC");
 
 //store in result
 
