87e00398d6fa2e21600c75d59d85b98c9a5f07cc
Emmanuel <emmanuel.nocum@cvsu.edu.ph>
2022-11-01 14:15:05 +0800

    zxczxczxc

---
+++ employee/backend/student-table.php
@@ -0,0 +1,22 @@
+<?php
+try{
+    header('Access-Control-Allow-Origin: *');
+    header('Content-type: application/json');
+    include_once("../connections/connection.php");
+$con = connection();
+
+
+$sql = mysqli_query($con, "SELECT 
+id, profile_url, studentnumber, firstname, middlename,lastname, email,address,sex,course,section, birthday,contact,guardian,guardian_contact,balance,status
+,added_at FROM `tbl_admin` ORDER BY `added_at` DESC");
+
+//store in result
+
+$result = mysqli_fetch_all($sql, MYSQLI_ASSOC);
+
+exit(json_encode($result));
+}catch (Exception $e){
+    exit(json_encode(array("statusCode"=>$e->getMessage())));
+}
+
+?>
