38bece44a732525f6c27ef07032bca8f1f59a15b
Emmanuel <emmanuel.nocum@cvsu.edu.ph>
2022-11-02 15:03:05 +0800

    zxvzvzxv

--- employee/backend/student-number-4digit-generate.php
+++ employee/backend/student-number-4digit-generate.php
@@ -6,15 +6,10 @@ include_once("../connections/connection.php");
 $con = connection();
 
 
-//Add shits here
-$Course = $_POST['Course'];
-$Year = $_POST['Year'];
-$AcademicYear = $_POST['AcademicYear'];
-
 
 try{
 
-    $existSQL = "SELECT * FROM tbl_studentinfo'";
+    $existSQL = "SELECT * FROM tbl_studentinfo";
     if($dataAdd = mysqli_query($con, $existSQL)){
         $rowcount = mysqli_num_rows($dataAdd);
         exit(json_encode(array("statusCode"=>$rowcount)));
