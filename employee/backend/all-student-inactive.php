8b8f590305000035bea5db83a935a9fc663fa0b7
Emmanuel <emmanuel.nocum@cvsu.edu.ph>
2022-11-06 17:18:19 +0800

    asdvas

---
+++ employee/backend/all-student-inactive.php
@@ -0,0 +1,31 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Content-type: application/json');
+include_once("../connections/connection.php");
+$con = connection();
+
+
+$sql = mysqli_query($con, "SELECT id,
+profile_url,
+studentnumber,
+firstname,
+middlename,
+lastname,
+email,
+address,
+course,
+section,
+birthday,
+contact,
+guardian,
+guardian_contact,
+balance,
+status,
+added_at FROM `tbl_studentinfo` WHERE status ='inactive' ORDER BY `added_at` DESC");
+
+//store in result
+
+$result = mysqli_fetch_all($sql, MYSQLI_ASSOC);
+
+exit(json_encode($result));
+?>
