9202c0185b9ced6a4c942b6996fd3135cb0f9b09
Emmanuel <emmanuel.nocum@cvsu.edu.ph>
2022-11-10 21:08:17 +0800

    zxvzxvz

--- employee/backend/login.php
+++ employee/backend/login.php
@@ -21,22 +21,28 @@ $con = connection();
     if (mysqli_num_rows($checkLoginEmail) == 0) {
       exit(json_encode(array("statusCode"=>201)));
   } else {
-    $row = mysqli_fetch_array($checkLoginEmail);
-    if (password_verify($password, $row['password'])) {
-      $sql = "SELECT `profile_url`,`email`,`firstname`,`middlename`,`lastname`,`birthday`,`sex`,`position`,`address`,`contact`,`about`,`twitterprofile`,`facebookprofile`,`instagramprofile`,`linkedinprofile`,`status`,`added_at` FROM tbl_admin WHERE email = '$email'  AND status = 'active'";
-    $user = $con ->query($sql) or die ($con->error);
-    $row = $user->fetch_assoc();
-    $total = $user->num_rows;
-  
-    if($total > 0){
-    $_SESSION['ID'] = session_id();
-      exit(json_encode(array("statusCode"=>$row)));
-    }else{
-      exit(json_encode(array("statusCode"=>201)));
-   }
-  }
+    try{
+      $row = mysqli_fetch_array($checkLoginEmail);
+      if (password_verify($password, $row['password'])) {
+        $sql = "SELECT `profile_url`,`email`,`firstname`,`middlename`,`lastname`,`birthday`,`sex`,`position`,`address`,`contact`,`about`,`twitterprofile`,`facebookprofile`,`instagramprofile`,`linkedinprofile`,`status`,`added_at` FROM tbl_admin WHERE email = '$email'  AND status = 'active'";
+      $user = $con ->query($sql) or die ($con->error);
+      $row = $user->fetch_assoc();
+      $total = $user->num_rows;
+    
+      if($total > 0){
+      $_SESSION['ID'] = session_id();
+        exit(json_encode(array("statusCode"=>$row)));
+      }else{
+        exit(json_encode(array("statusCode"=>201)));
+     }
+    }
+    }catch(Exception $e){
+      exit(json_encode(array("statusCode"=>$e->getMessage())));
+    }
+   
  }
 }
+
 }catch(Exception $e){
   exit(json_encode(array("statusCode"=>$e->getMessage())));
 }
