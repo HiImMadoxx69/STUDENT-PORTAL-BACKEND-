187a984daf0ff4b5f9c3d2ca1800ad41df8041c2
Emmanuel <emmanuel.nocum@cvsu.edu.ph>
2022-11-11 16:21:08 +0800

    dfhdfhdfhf

--- employee/backend/student-add.php
+++ employee/backend/student-add.php
@@ -60,82 +60,8 @@ $mail->setFrom('nocumadoxx@gmail.com');
 //Enable HTML
 $mail->isHTML(true);
 //Email body
-$mail->Body = "<body width="100%" style="margin: 0; padding: 0 !important; mso-line-height-rule: exactly; background-color: #f1f1f1;">
-<center style="width: 100%; background-color: #f1f1f1;">
-<div style="display: none; font-size: 1px;max-height: 0px; max-width: 0px; opacity: 0; overflow: hidden; mso-hide: all; font-family: sans-serif;">
-  &zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;
-</div>
-<div style="max-width: 600px; margin: 0 auto;" class="email-container">
-    <!-- BEGIN BODY -->
-  <table align="center" role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%" style="margin: auto;">
-      <tr>
-      <td valign="top" class="bg_white" style="padding: 1em 2.5em 0 2.5em;">
-          <table role="presentation" border="0" cellpadding="0" cellspacing="0" width="100%">
-              <tr>
-                  <td class="logo" style="text-align: center;">
-                    <h1><a href="#">AISAT COLLEGE DASMARIÑAS</a></h1>
-                  </td>
-              </tr>
-          </table>
-      </td>
-      </tr><!-- end tr -->
-            <tr>
-      <td valign="middle" class="hero bg_white" style="padding: 2em 0 4em 0;">
-        <table role="presentation" border="0" cellpadding="0" cellspacing="0" width="100%">
-            <tr>
-                <td style="padding: 0 2.5em; text-align: center; padding-bottom: 3em;">
-                    <div class="text">
-                        <h2>Welcome to Asian Institute of Technology Dasmariñas!</h2>
-                    </div>
-                </td>
-            </tr>
-            <tr>
-                  <td style="text-align: center;">
-                      <div class="text-author">
-                          <img src="images/logo.jpg" alt="" style="width: 100px; max-width: 600px; height: auto; margin: auto; display: block;">
-                          <h2 class="name">WELCOME NEW PROFFESOR!</h2>
-                           <p><a href="https://aisat-portal.netlify.app/?fbclid=IwAR3_LTplTpx1zRLeUxtWCQkJV0_FEBfh0PzkduGepNm5cLzKWVwRcFWf_AQ" class="btn btn-primary">AISAT HOME PAGE</a></p>
-                           <p><a href="https://aisat-portal.netlify.app/Loginemployee?fbclid=IwAR2oAnVixrwy09Sdl6Td2N6m3R42m5_kaNG3v0gXO5i7LkfbZN184oTCrP4" class="btn-custom">Portal Login Page</a></p>
-                       </div>
-                  </td>
-                </tr>
-        </table>
-      </td>
-      </tr><!-- end tr -->
-  <!-- 1 Column Text + Button : END -->
-  </table>
-  <table align="center" role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%" style="margin: auto;">
-      <tr>
-      <td valign="middle" class="bg_light footer email-section">
-        <table>
-            <tr>
-            <td valign="top" width="33.333%" style="padding-top: 20px;">
-              <table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%">
-                <tr>
-                  <td style="text-align: left; padding-right: 10px;">
-                      <h3 class="heading">You can now Login to our Portal!</h3>
-                      <p>EMAIL: = $Email</p>
-                    <p>PASSWORD: = $Password</p>
-                    <p>If you are not able to login your account please contact the registar on the campus.</p>
-                  </td>
-                </tr>
-              </table>
-            </td>
-
-          </tr>
-        </table>
-      </td>
-    </tr><!-- end: tr -->
-    <tr>
-      <td class="bg_light" style="text-align: center;">
-          
-      </td>
-    </tr>
-  </table>
-
-</div>
-</center>
-</body>";
+$mail->Body ="<h1>HI! Our School Now Have A New Student! </h1> <h2>Welcome to our Family!</h2>
+<p>You can now login to our Student portal: Your Password = $Password</p>";
 //Add recipient
 $mail->addAddress($Email);
 //Finally send email
