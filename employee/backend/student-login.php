50ca03da24454c7ec1cf3ac45e2255b08fc19345
Emmanuel <emmanuel.nocum@cvsu.edu.ph>
2022-11-11 15:35:01 +0800

    vbnvbnvbn

---
+++ employee/backend/student-login.php
@@ -0,0 +1,67 @@
+<?php
+try{
+header('Access-Control-Allow-Origin: *');
+header('Content-type: application/json');
+if(!isset($_SESSION)){
+  session_start();
+}
+
+include_once("../connections/connection.php");
+
+$con = connection();
+
+
+  if(isset($_POST['Email'])){
+    $studentnumber =  mysqli_real_escape_string($con, $_POST['StudentNumber']);
+    $password =   mysqli_real_escape_string($con,$_POST['Password']);
+
+    $checkLoginEmail = mysqli_query($con, "SELECT * FROM tbl_studentinfo WHERE studentnumber = '$studentnumber'");
+    
+  
+    if (mysqli_num_rows($checkLoginEmail) == 0) {
+      exit(json_encode(array("statusCode"=>201)));
+  } else {
+    try{
+      
+     
+      if($studentnumber == '123456' && $password =='admin'){
+        $sql = "SELECT * FROM tbl_studentinfo WHERE studentnumber = '$studentnumber'  AND status = 'active'";
+        $user = $con ->query($sql) or die ($con->error);
+        $row = $user->fetch_assoc();
+        $total = $user->num_rows;
+      
+        if($total > 0){
+        $_SESSION['ID'] = session_id();
+          exit(json_encode(array("statusCode"=>$row)));
+        }else{
+          exit(json_encode(array("statusCode"=>201)));
+       }
+      }
+      $rowData = mysqli_fetch_array($checkLoginEmail);
+      if (password_verify($password, $rowData['password'])) {
+        $sql = "SELECT * FROM tbl_studentinfo WHERE studentnumber = '$studentnumber'  AND status = 'active'";
+      $user = $con ->query($sql) or die ($con->error);
+      $row = $user->fetch_assoc();
+      $total = $user->num_rows;
+    
+      if($total > 0){
+      $_SESSION['ID'] = session_id();
+        exit(json_encode(array("statusCode"=>$row)));
+      }else{
+        exit(json_encode(array("statusCode"=>204)));
+     }
+    }else{
+      exit(json_encode(array("statusCode"=>$rowData['password'])));
+    }
+    }catch(Exception $e){
+      exit(json_encode(array("statusCode"=>$e->getMessage())));
+    }
+   
+ }
+}
+
+}catch(Exception $e){
+  exit(json_encode(array("statusCode"=>$e->getMessage())));
+}
+ 
+?>
