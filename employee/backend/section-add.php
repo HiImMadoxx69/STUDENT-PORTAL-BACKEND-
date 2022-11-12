620710612a479a32c363ba19c1089707391017a3
Emmanuel <emmanuel.nocum@cvsu.edu.ph>
2022-11-12 17:00:27 +0800

    zxvzxv

--- employee/backend/section-add.php
+++ employee/backend/section-add.php
@@ -49,7 +49,7 @@ try{
         $GenerateDesc = $result[$i]['subject_name'];
         $GenerateUnit = $result[$i]['units'];
         $GenerateID = $result[$i]['id'];
-        $GenerateSectionAY = $SectionName+" " +$AcademicYear;
+        $GenerateSectionAY = $SectionName . $AcademicYear;
         $sqlGenerateSchedule = "INSERT INTO tbl_subjectpersection (`subject_name`, `section_name`, `description`, `units`,`semester`, `schedule_day`, `schedule_time`, `professor_initial`, `academic_year`,`sectionacademicyear`, `subject_id`,`section_id`) VALUES ('$GenerateSubName', '$SectionName', '$GenerateDesc', '$GenerateUnit','$Semester', '', '', '', '$AcademicYear','$GenerateSectionAY' '$GenerateID','$GenerateSectionID');";
         mysqli_query($con, $sqlGenerateSchedule);
     }
