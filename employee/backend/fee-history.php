bb4e418e1633cdb5239eada51c6f152e15f9eefe
Emmanuel <emmanuel.nocum@cvsu.edu.ph>
2022-11-20 20:13:43 +0800

    xcbxcb

---
+++ employee/backend/fee-history.php
@@ -0,0 +1,21 @@
+<?php
+ header('Access-Control-Allow-Origin: *');
+ header('Content-type: application/json');
+include_once("../connections/connection.php");
+$con = connection();
+
+try{
+    
+    $sql = mysqli_query($con, "SELECT 
+    * FROM `tbl_updatehistory` WHERE `category` = 'Fee'");
+    
+    //store in result
+    
+    $result = mysqli_fetch_all($sql, MYSQLI_ASSOC);
+    
+    exit(json_encode($result));
+}catch(Exception $e){
+    exit(json_encode(array($e->getMessage())));
+}
+
+?>
