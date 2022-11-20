9a24367be6c98f32677a8955a793bf328f2618cb
Emmanuel <emmanuel.nocum@cvsu.edu.ph>
2022-11-20 19:59:11 +0800

    cvbcvb

---
+++ employee/backend/fee-update.php
@@ -0,0 +1,62 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Content-type: application/json');
+include_once("../connections/connection.php");
+$con = connection();
+
+try{
+
+$CurrentId  = $_POST['ID'];
+$Name = $_POST['Name'];
+$Amount = $_POST['Amount'];
+$Status = $_POST['Status'];
+$Action = $_POST['Action'];
+$EditorPosition = $_POST['EditorPosition'];
+$EditorEmail = $_POST['EditorEmail'];
+$Category = $_POST['Category'];
+$Type = $_POST['Type'];
+$Course = $_POST['Course'];
+$Year = $_POST['Year'];
+$Semester = $_POST['Semester'];
+//sd
+try{
+    
+    if (isset($CurrentId)) {
+
+        $beforeSql = "SELECT * FROM tbl_fee WHERE `id` = '$CurrentId';";     
+       
+        mysqli_query($con, $beforeSql);
+
+        $getBefore = $con ->query($beforeSql) or die ($con->error);
+        $setBefore =  $getBefore ->fetch_assoc();
+        $rowBefore = json_encode($setBefore);
+
+        $sql = "UPDATE `tbl_fee` SET `name` = '$Name', `amount` = '$Amount',`status` = '$Status' WHERE `tbl_fee`.`id` = '$CurrentId';";
+        mysqli_query($con, $sql);
+       
+        $AfterSql = "SELECT * FROM tbl_fee WHERE `id` = '$CurrentId';";     
+                
+        mysqli_query($con, $AfterSql);
+
+        $getAfter = $con ->query($AfterSql) or die ($con->error);
+        $rowAfter = json_encode($getAfter ->fetch_assoc());
+        
+
+        $auditsql = "INSERT INTO `tbl_updatehistory` (`action`,`category`,`editor_position`,`editor_email`,`edited_email`,`after_edit`,`before_edit`) VALUES ('$Action','$Category','$EditorPosition','$Name', '$SubjectCode', '$rowAfter','$rowBefore');";
+                  mysqli_query($con, $auditsql);
+
+                  $xsql = "SELECT * from `tbl_fee` WHERE `id` =  '$CurrentId'";
+        mysqli_query($con, $xsql);
+
+        $user = $con ->query($xsql) or die ($con->error);
+        $row = $user->fetch_assoc();
+        exit(json_encode(array("statusCode"=>$row)));
+    }
+}catch(Exception $e){
+    exit(json_encode(array("statusCode"=>$e->getMessage())));
+}
+
+}catch(Exception $e){
+    exit(json_encode(array("statusCode"=>$e->getMessage())));
+}
+?>
