069ad84f90e913d826e07ed78fdb6a27b3dbed04
Emmanuel <emmanuel.nocum@cvsu.edu.ph>
2022-11-21 16:07:55 +0800

    xcvxcvxc

--- employee/backend/curriculum-per-year.php
+++ employee/backend/curriculum-per-year.php
@@ -11,12 +11,12 @@ $Year = $_POST['Year'];
 try{
     
 
-    $sql = mysqli_query($con, "SELECT * FROM `tbl_subject` WHERE status = 'active' AND `course_avaialable` LIKE '%$Course%' AND `year_available` = '$Year' ORDER BY `id` DESC");
+    $sql = mysqli_query($con, "SELECT * FROM `tbl_subject` WHERE status = 'active' AND `course_available` LIKE '%$Course%' AND `year_available` = '$Year' ORDER BY `id` DESC");
 
     //store in result
     
     $result = mysqli_fetch_all($sql, MYSQLI_ASSOC);
-
+    exit(json_encode($result));
   
 }catch(Exception $e){
     exit(json_encode(array("statusCode"=>$e->getMessage())));
