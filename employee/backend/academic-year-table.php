dacf6464cfc81e741cee1cd8c3fa5cabce473917
Emmanuel <emmanuel.nocum@cvsu.edu.ph>
2022-11-21 19:03:10 +0800

    cvbcvbcvb

---
+++ employee/backend/academic-year-table.php
@@ -0,0 +1,16 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Content-type: application/json');
+
+include_once("../connections/connection.php");
+$con = connection();
+
+
+$sql = mysqli_query($con, "SELECT * FROM `tbl_academicyear`  ORDER BY `id` DESC");
+
+//store in result
+
+$result = mysqli_fetch_all($sql, MYSQLI_ASSOC);
+
+exit(json_encode($result));
+?>
