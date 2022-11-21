304fbdeca51843c4ec623add574a8e54670d20e2
Emmanuel <emmanuel.nocum@cvsu.edu.ph>
2022-11-21 15:58:54 +0800

    fgnfgn

---
+++ employee/backend/curriculum-per-year.php
@@ -0,0 +1,26 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Content-type: application/json');
+
+include_once("../connections/connection.php");
+$con = connection();
+
+$Course = $_POST['Course'];
+$Year = $_POST['Year'];
+
+try{
+    
+
+    $sql = mysqli_query($con, "SELECT * FROM `tbl_subject` WHERE status = 'active' AND `course_avaialable` LIKE '%$Course%' AND `year_available` = '$Year' ORDER BY `id` DESC");
+
+    //store in result
+    
+    $result = mysqli_fetch_all($sql, MYSQLI_ASSOC);
+
+  
+}catch(Exception $e){
+    exit(json_encode(array("statusCode"=>$e->getMessage())));
+}
+
+
+?>
