1082005acc586ae01a1c1401bf006e72006f0453
Emmanuel <emmanuel.nocum@cvsu.edu.ph>
2022-11-23 12:53:20 +0800

    zxvzxv

--- quickstart.php
+++ quickstart.php
@@ -21,29 +21,33 @@ $img_size = $_FILES['Image']['size'];
 $imgSize = $_FILES['Image']['size'];
 $tmp_name = $_FILES['Image']['tmp_name'];
 
+try{
 
-if (isset($img_name)) {
+    if (isset($img_name)) {
 
   
-    if ( $imgSize > 2000000) {
-      exit(json_encode(array("statusCode"=>201)));
-    }
-  
-    if (empty($errors)) {
-  
-                    $img_ex = pathinfo($img_name, PATHINFO_EXTENSION);
-                    $img_ex_lc = strtolower($img_ex);
-  
-                    $new_img_name = uniqid("IMG-", true).'.'.$img_ex_lc;
-                    $img_upload_path = 'uploads/'.$new_img_name;
-                    move_uploaded_file($tmp_name, $img_upload_path);
+        if ( $imgSize > 2000000) {
+          exit(json_encode(array("statusCode"=>201)));
+        }
       
-                   
-                    exit(json_encode(array("image"=>$new_img_name,"statusCode"=>200)));
-    } else {
-      exit(json_encode(array("statusCode"=>201)));
-    }
-  } 
+        if (empty($errors)) {
+      
+                        $img_ex = pathinfo($img_name, PATHINFO_EXTENSION);
+                        $img_ex_lc = strtolower($img_ex);
+      
+                        $new_img_name = uniqid("IMG-", true).'.'.$img_ex_lc;
+                        $img_upload_path = 'uploads/'.$new_img_name;
+                        move_uploaded_file($tmp_name, $img_upload_path);
+          
+                       
+                        exit(json_encode(array("image"=>$new_img_name,"statusCode"=>200)));
+        } else {
+          exit(json_encode(array("statusCode"=>201)));
+        }
+      } 
+}catch(Exception $e){
+    exit(json_encode(array("statusCode"=>$e->getMessage())));
+}
 
 
 // // Upload the image
