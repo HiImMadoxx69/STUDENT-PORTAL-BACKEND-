f3ebfb42cd210e92818c94f16adc758632d09fe6
Emmanuel <emmanuel.nocum@cvsu.edu.ph>
2022-11-24 21:42:56 +0800

    vbnvb

---
+++ employee/backend/admin-profile-edit.php
@@ -0,0 +1,41 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Content-type: application/json');
+
+include_once("../connections/connection.php");
+$con = connection();
+
+
+
+$FirstName = $_POST['FirstName'];
+$MiddleName = $_POST['MiddleName'];
+$LastName = $_POST['LastName'];
+$Birthday = $_POST['Birthday'];
+$Contact = $_POST['Contact'];
+$Address = $_POST['Address'];
+$ImageUrl = $_POST['ImageUrl'];
+$Email = $_POST['Email'];
+try{
+    
+
+
+    $sql = "UPDATE `tbl_admin` SET `firstname` = '$FirstName', `middlename` = '$MiddleName',`lastname` = '$LastName', `birthday` = '$Birthday', `contact` = '$Contact', `address` = '$Address', `profile_url` = '$ImageUrl' WHERE `tbl_admin`.`firstname` = '$FirstName';";
+    mysqli_query($con, $sql);
+
+    $AfterSql = "SELECT * FROM tbl_admin WHERE `email` = '$Email';";     
+                
+    mysqli_query($con, $AfterSql);
+
+    $getAfter = $con ->query($AfterSql) or die ($con->error);
+    $rowAfter = json_encode($getAfter ->fetch_assoc());
+
+   
+    exit(json_encode(array("statusCode"=>200, "information" => $rowAfter )));
+
+  
+}catch(Exception $e){
+    exit(json_encode(array("statusCode"=>201, "error" => $e->getMessage())));
+}
+
+
+?>
