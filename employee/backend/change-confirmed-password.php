1b4d27577ab7969b17dfd8b0365b73223cb9cbbe
Emmanuel <emmanuel.nocum@cvsu.edu.ph>
2022-11-24 18:21:43 +0800

    dfgdfgd

---
+++ employee/backend/change-confirmed-password.php
@@ -0,0 +1,34 @@
+<?php
+ header('Access-Control-Allow-Origin: *');
+ header('Content-type: application/json');
+include_once("../connections/connection.php");
+$con = connection();
+
+
+try{
+
+    $Email = $_POST['Email'];
+    $Password = $_POST['Password'];
+  
+if (isset($email)) {
+
+ 
+ try{
+    $hashedPassword = password_hash($Password, PASSWORD_DEFAULT);
+
+                  $sql = "UPDATE `tbl_admin` SET `password` = '$hashedPassword' WHERE `tbl_admin`.`email` = '$Email';";
+    
+                  mysqli_query($con, $sql);
+
+
+        exit(json_encode(array("statusCode"=>200)));
+ }catch(Exception $e){
+  exit(json_encode(array("statusCode"=>$e->getMessage())));
+  // exit(json_encode(array("statusCode"=>201)));
+ }
+
+}
+}catch(Exception $e){
+  // exit(json_encode(array("statusCode"=>$e->getMessage())));
+  exit(json_encode(array("statusCode"=>201)));
+}
