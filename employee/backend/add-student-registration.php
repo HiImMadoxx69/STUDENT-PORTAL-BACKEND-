8b54543d39d296df8a2ac06adf25bc45ee01f2e3
Emmanuel <emmanuel.nocum@cvsu.edu.ph>
2022-11-27 19:29:49 +0800

    cvbcvbcv

--- employee/backend/add-student-registration.php
+++ employee/backend/add-student-registration.php
@@ -5,6 +5,10 @@ header('Content-type: application/json');
 include_once("../connections/connection.php");
 $con = connection();
 
+$StudentNumber = $_POST['StudentNumber'];
+$AcademicYear = $_POST['AcademicYear'];
+$SectionAndYear = $_POST['SectionAndSemester'];
+$Semester = $_POST['Semester'];
 $Schedule = $_POST['Schedule'];
 $StudentNumber = $_POST['StudentNumber'];
 $Fee = $_POST['Fee'];
@@ -12,6 +16,8 @@ $GetSchedule = json_decode($Schedule, true);
 $GetFee = json_decode($Fee, true);
 try{
 
+    $sqlRegister = "INSERT INTO tbl_studentregistration (`student_id`,`sectionandsemester`,`academicyear`,`semester`,`sectionandyear`) VALUES ('$StudentNumber', '$SectionAndYear', '$AcademicYear', '$Semester', '$SectionAndYear');";
+mysqli_query($con, $sqlGenerateSchedule);
 
 for($i = 0; $i < count($GetSchedule); $i++ ){ 
 $Sched = $GetSchedule[$i]['sched_code'];
@@ -22,7 +28,6 @@ $Units = $GetSchedule[$i]['units'];
 $Semester = $GetSchedule[$i]['semester'];
 $Day = $GetSchedule[$i]['schedule_day'];
 $Time = $GetSchedule[$i]['schedule_time'];
-$AcademicYear = $GetSchedule[$i]['academic_year'];
 $SectionAndYear = $GetSchedule[$i]['sectionacademicyear'];
 
 $sqlGenerateSchedule = "INSERT INTO tbl_gradesperstudent (`sched_code`, `student_id`,`subject_name`,`section_name`,`description`,`units`,`semester`,`schedule_day`,`schedule_time`,`academic_year`,`sectionandacademicyear`) VALUES ('$Sched', '$StudentNumber', '$SubjectName', '$SectionName', '$Description', '$Units', '$Semester', '$Day', '$Time', '$AcademicYear','$SectionAndYear');";
@@ -35,7 +40,7 @@ for($i = 0; $i < count($GetFee[0]); $i++){
     $Amount = $GetFee[0][$i]['amount'];
     $Subtotal = $GetFee[0][$i]['subtotal'];
     $sqlAddStudentFee ="INSERT INTO tbl_feeperstudent
-    (`name`,`student_id`, `amount`,`subtotal`) VALUES ('$Name', '$StudentNumber', '$Amount','$Subtotal');";
+    (`name`,`student_id`, `amount`,`subtotal`,`academicyear`,`sectionandyear`) VALUES ('$Name', '$StudentNumber', '$Amount','$Subtotal','$AcademicYear','$SectionAndYear');";
     mysqli_query($con, $sqlAddStudentFee);    
 }
 
