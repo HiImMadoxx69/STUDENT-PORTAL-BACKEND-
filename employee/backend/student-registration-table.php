6270bf7ff08a93f458456fca405d14fabfa61f1a
Emmanuel <emmanuel.nocum@cvsu.edu.ph>
2022-11-28 20:33:34 +0800

    xcvxcv

--- employee/backend/student-registration-table.php
+++ employee/backend/student-registration-table.php
@@ -5,7 +5,7 @@ try{
     include_once("../connections/connection.php");
 $con = connection();
 
-$sql = mysqli_query($con, "SELECT `tbl_studentregistration`.* , `tbl_studentinfo`.`firstname`,`tbl_studentinfo`.`middlename`, `tbl_studentinfo`.`lastname`, `tbl_studentinfo`.`profile_url`, `tbl_feeperstudent`.*, `tbl_gradesperstudent`.* FROM `tbl_studentregistration` LEFT JOIN `tbl_studentinfo` ON `tbl_studentregistration`.`student_id` = `tbl_studentinfo`.`studentnumber` LEFT JOIN `tbl_feeperstudent` ON `tbl_studentregistration`.`sectionandsemester` = `tbl_feeperstudent`.`sectionandsemester` LEFT JOIN `tbl_gradesperstudent` ON `tbl_studentregistration`.`sectionandsemester` = `tbl_gradesperstudent`.`sectionandsemester` ORDER BY `tbl_studentregistration`.`added_at`;");
+$sql = mysqli_query($con, "SELECT `tbl_studentregistration`.* , `tbl_studentinfo`.`firstname`,`tbl_studentinfo`.`middlename`, `tbl_studentinfo`.`lastname`, `tbl_studentinfo`.`profile_url` FROM `tbl_studentregistration` LEFT JOIN `tbl_studentinfo` ON `tbl_studentregistration`.`student_id` = `tbl_studentinfo`.`studentnumber` ORDER BY `tbl_studentregistration`.`added_at`;");
 
 //store in result
 
