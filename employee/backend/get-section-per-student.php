6856077e2ef3e8162e74b1597bada4e409ed5076
Emmanuel <emmanuel.nocum@cvsu.edu.ph>
2022-11-29 22:00:07 +0800

    cvbcvbcvdfbd

--- employee/backend/get-section-per-student.php
+++ employee/backend/get-section-per-student.php
@@ -8,13 +8,11 @@ $con = connection();
 
 $StudentID = $_POST['StudentId'];
 
-    $sql = "SELECT `tbl_studentinfo`.* , `tbl_section`.* FROM `tbl_studentinfo` LEFT JOIN `tbl_section` ON `tbl_studentinfo`.`section` = `tbl_section`.`sectionandsemester` WHERE `tbl_studentinfo`.`studentnumber` = '$StudentID';";
+    $sql = mysqli_query($con,"SELECT `tbl_studentinfo`.* , `tbl_section`.* FROM `tbl_studentinfo` LEFT JOIN `tbl_section` ON `tbl_studentinfo`.`section` = `tbl_section`.`sectionandsemester` WHERE `tbl_studentinfo`.`studentnumber` = '$StudentID'");
 
-    $user = $con ->query($sql) or die ($con->error);
-    $row = $user->fetch_assoc();
-
+    $result = mysqli_fetch_all($sql, MYSQLI_ASSOC);
   
-    exit(json_encode(array("statusCode"=>200, "content" => $row)));
+    exit(json_encode(array("statusCode"=>200, "content" => $result)));
  }catch(Exception $e){
     exit(json_encode(array("statusCode" =>201, "error"=>$e->getMessage())));
  }
