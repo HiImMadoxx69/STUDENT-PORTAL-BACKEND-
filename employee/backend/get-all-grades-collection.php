07cd774e36050c83b4ac11c42bb172da725d135d
Emmanuel <emmanuel.nocum@cvsu.edu.ph>
2022-11-30 09:31:00 +0800

    fgnfgn

--- employee/backend/get-all-grades-collection.php
+++ employee/backend/get-all-grades-collection.php
@@ -8,18 +8,13 @@ $con = connection();
 
 $StudentID = $_POST['StudentId'];
 
-    $sql = "SELECT * FROM tbl_studentregistration WHERE student_id = '$StudentID' AND status = 'confirmed' ";
+$sql = mysqli_query($con,"SELECT * FROM tbl_studentregistration WHERE student_id = '$StudentID' AND status = 'confirmed';");
 
-    $user = $con ->query($sql) or die ($con->error);
-    $row = $user->fetch_assoc();
-    $total = $user->num_rows;
+    
+    $result = mysqli_fetch_all($sql, MYSQLI_ASSOC);
   
-    if($total > 0){
-        exit(json_encode(array("statusCode"=>200)));
-    }else{
-        exit(json_encode(array("statusCode"=>'Schedule doesnt not exist')));
-    }
-    exit(json_encode(array("statusCode"=>201)));
+    exit(json_encode(array("statusCode"=>200, "content" => $result)));
+    
  }catch(Exception $e){
     exit(json_encode(array("statusCode"=>201)));
  }
