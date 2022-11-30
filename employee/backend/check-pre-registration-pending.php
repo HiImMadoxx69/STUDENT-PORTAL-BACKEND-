36eec5779ecda79f4e9de26d6d3d80b12eac3045
Emmanuel <emmanuel.nocum@cvsu.edu.ph>
2022-12-01 03:33:07 +0800

    fghfghfghfghfg

---
+++ employee/backend/check-pre-registration-pending.php
@@ -0,0 +1,20 @@
+<?php
+ header('Access-Control-Allow-Origin: *');
+ header('Content-type: application/json');
+
+ try{
+    include_once("../connections/connection.php");
+$con = connection();
+
+$StudentID = $_POST['StudentId'];
+
+    $sql = mysqli_query($con,"SELECT * FROM `tbl_studentregistration` WHERE `tbl_studentregistration`.`student_id` = '$StudentID';");
+
+    $result = mysqli_fetch_all($sql, MYSQLI_ASSOC);
+  
+    exit(json_encode(array("statusCode"=>200, "content" => $result)));
+ }catch(Exception $e){
+    exit(json_encode(array("statusCode" =>201, "error"=>$e->getMessage())));
+ }
+
+ ?>
