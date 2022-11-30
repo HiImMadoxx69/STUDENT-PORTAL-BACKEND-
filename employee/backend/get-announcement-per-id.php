a60ac053f024aa1d31a6d1e98fe4aa3ac10682da
Emmanuel <emmanuel.nocum@cvsu.edu.ph>
2022-11-30 16:22:06 +0800

    dfbfdb

---
+++ employee/backend/get-announcement-per-id.php
@@ -0,0 +1,22 @@
+<?php
+ header('Access-Control-Allow-Origin: *');
+ header('Content-type: application/json');
+
+ try{
+    include_once("../connections/connection.php");
+$con = connection();
+
+$ID = $_POST['ID'];
+
+$sql = mysqli_query($con,"SELECT * FROM tbl_announcement WHERE id = '$ID';");
+
+    
+    $result = mysqli_fetch_all($sql, MYSQLI_ASSOC);
+  
+    exit(json_encode(array("statusCode"=>200, "content" => $result)));
+    
+ }catch(Exception $e){
+    exit(json_encode(array("statusCode"=>201)));
+ }
+
+ ?>
