99e37d52fc40e33a07643f73047d515fb52a1b54
Emmanuel <emmanuel.nocum@cvsu.edu.ph>
2022-11-30 13:28:14 +0800

    wwer

--- employee/backend/add-student-registration.php
+++ employee/backend/add-student-registration.php
@@ -35,14 +35,23 @@ mysqli_query($con, $sqlGenerateSchedule);
 
 }
 
+$TotalFee = 0;
+$TotalPaid = 0;
+$Balance = 0;
 for($i = 0; $i < count($GetFee[0]); $i++){
     $Name = $GetFee[0][$i]['name'];
     $Amount = $GetFee[0][$i]['amount'];
     $Subtotal = $GetFee[0][$i]['subtotal'];
+    $TotalFee = doubleval($TotalFee) + doubleval($Subtotal);
     $sqlAddStudentFee ="INSERT INTO tbl_feeperstudent
     (`name`,`student_id`, `amount`,`subtotal`,`academicyear`,`sectionandyear`,`sectionandsemester`) VALUES ('$Name', '$StudentNumber', '$Amount','$Subtotal','$AcademicYear','$SectionAndYear', '$SectionAndSemester');";
     mysqli_query($con, $sqlAddStudentFee);    
 }
+
+$sqlAddStudentFee ="INSERT INTO tbl_accountbalance
+(`studentnumber`,`academicyear`,`semester`,`totalfee`,`totalpaid`,`balance`) VALUES ('$StudentNumber', '$AcademicYear', '$Semester','$TotalFee','$TotalPaid','$Balance');";
+mysqli_query($con, $sqlAddStudentFee); 
+
 
 exit(json_encode(array("statusCode"=>200)));
 }catch(Exception $e){
