4dae6df50a6a58861901115d103ae99509ce65dc
Emmanuel <emmanuel.nocum@cvsu.edu.ph>
2022-11-30 11:33:01 +0800

    xcvxcvxcv

--- employee/backend/get-grades-per-student.php
+++ employee/backend/get-grades-per-student.php
@@ -9,11 +9,11 @@ $con = connection();
 $StudentID = $_POST['StudentId'];
 $SectionAndSemester = $_POST['SectionAndSemester'];
 
-    $sql = mysqli_query($con,"SELECT * FROM `tbl_gradesperstudent` WHERE `tbl_gradesperstudent`.`student_id` = '$StudentID' AND `tbl_gradesperstudent`.`sectionandsemester` = '$SectionAndSemester ' ;");
+    $sql = mysqli_query($con,"SELECT * FROM `tbl_gradesperstudent` WHERE `tbl_gradesperstudent`.`student_id` = '$StudentID' AND `tbl_gradesperstudent`.`sectionandsemester` = '$SectionAndSemester' ;");
 
     $result = mysqli_fetch_all($sql, MYSQLI_ASSOC);
    
-    $sumSql = mysqli_query($con,"SELECT SUM(`grade`) WHERE `tbl_gradesperstudent`.`student_id` = '$StudentID' AND `tbl_gradesperstudent`.`sectionandsemester` = '$SectionAndSemester ' ;");
+    $sumSql = mysqli_query($con,"SELECT SUM(`grade`) FROM `tbl_gradesperstudent` WHERE `tbl_gradesperstudent`.`student_id` = '$StudentID' AND `tbl_gradesperstudent`.`sectionandsemester` = '$SectionAndSemester';");
 
     $GPA = mysqli_fetch_all($sumSql, MYSQLI_ASSOC);
 
