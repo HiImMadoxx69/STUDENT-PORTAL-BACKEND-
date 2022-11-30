67ed1e06c9e903e0988665d8ba137e6740ceb001
Emmanuel <emmanuel.nocum@cvsu.edu.ph>
2022-11-30 11:28:13 +0800

    sdvsdvs

--- employee/backend/get-grades-per-student.php
+++ employee/backend/get-grades-per-student.php
@@ -12,8 +12,12 @@ $SectionAndSemester = $_POST['SectionAndSemester'];
     $sql = mysqli_query($con,"SELECT * FROM `tbl_gradesperstudent` WHERE `tbl_gradesperstudent`.`student_id` = '$StudentID' AND `tbl_gradesperstudent`.`sectionandsemester` = '$SectionAndSemester ' ;");
 
     $result = mysqli_fetch_all($sql, MYSQLI_ASSOC);
-  
-    exit(json_encode(array("statusCode"=>200, "content" => $result)));
+   
+    $sumSql = mysqli_query($con,"SELECT SUM(`grade`) WHERE `tbl_gradesperstudent`.`student_id` = '$StudentID' AND `tbl_gradesperstudent`.`sectionandsemester` = '$SectionAndSemester ' ;");
+
+    $GPA = mysqli_fetch_all($sumSql, MYSQLI_ASSOC);
+
+    exit(json_encode(array("statusCode"=>200, "content" => $result, "GPA" => $GPA)));
  }catch(Exception $e){
     exit(json_encode(array("statusCode" =>201, "error"=>$e->getMessage())));
  }
