462be655bfa05a096a26da39e4000a800067789b
Emmanuel <emmanuel.nocum@cvsu.edu.ph>
2022-12-01 12:47:22 +0800

    dfbfdb

---
+++ employee/backend/get-student-grade-per-section.php
@@ -0,0 +1,20 @@
+<?php
+ header('Access-Control-Allow-Origin: *');
+ header('Content-type: application/json');
+
+ try{
+    include_once("../connections/connection.php");
+$con = connection();
+
+$Username = $_POST['Username'];
+
+    $sql = mysqli_query($con,"SELECT `tbl_gradesperstudent`.* ,`tbl_studentinfo`.`firstname`, `tbl_studentinfo`.`middlename`, `tbl_studentinfo`.`lastname` FROM `tbl_gradesperstudent` LEFT JOIN `tbl_subjectpersection` ON `tbl_gradesperstudent`.`sectionandacademicyear` = `tbl_subjectpersection`.`sectionacademicyear` LEFT JOIN `tbl_studentinfo` ON `tbl_gradesperstudent`.`student_id` = `tbl_studentinfo`.`studentnumber` WHERE `tbl_subjectpersection`.`professor_initial` = '$Username';");
+
+    $result = mysqli_fetch_all($sql, MYSQLI_ASSOC);
+  
+    exit(json_encode(array("statusCode"=>200, "content" => $result)));
+ }catch(Exception $e){
+    exit(json_encode(array("statusCode" =>201, "error"=>$e->getMessage())));
+ }
+
+ ?>
