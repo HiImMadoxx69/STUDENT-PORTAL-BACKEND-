14cc00d48c043b0abcedfb42002593ddb0b037a9
Emmanuel <emmanuel.nocum@cvsu.edu.ph>
2022-12-01 11:26:38 +0800

    hsdhs

--- employee/backend/get-section-per-professor.php
+++ employee/backend/get-section-per-professor.php
@@ -8,7 +8,7 @@ $con = connection();
 
 $Email = $_POST['Email'];
 
-    $sql = mysqli_query($con,"SELECT `tbl_professor`.`email` , `tbl_subjectpersection`.* FROM `tbl_professor` LEFT JOIN `tbl_subjectpersection` ON `tbl_professor`.`professor_username` = `tbl_subjectpersection`.`professor_initial` WHERE `tbl_professor`.`email` = '$Email'  AND `tbl_subjectpersection`.`status` = 'active';");
+    $sql = mysqli_query($con,"SELECT DISTINCT `tbl_subjectpersection`.`sectionacademicyear` FROM `tbl_professor` LEFT JOIN `tbl_subjectpersection` ON `tbl_professor`.`professor_username` = `tbl_subjectpersection`.`professor_initial` WHERE `tbl_professor`.`email` = '$Email' AND `tbl_subjectpersection`.`status` = 'active';");
 
     $result = mysqli_fetch_all($sql, MYSQLI_ASSOC);
   
