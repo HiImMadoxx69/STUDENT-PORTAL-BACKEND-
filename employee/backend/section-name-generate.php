557128ad925b36d2d3342977d27c5f0c655cf537
Emmanuel <emmanuel.nocum@cvsu.edu.ph>
2022-12-02 18:26:13 +0800

    fgbfgbfgb

--- employee/backend/section-name-generate.php
+++ employee/backend/section-name-generate.php
@@ -10,11 +10,12 @@ $con = connection();
 $Course = $_POST['Course'];
 $Year = $_POST['Year'];
 $AcademicYear = $_POST['AcademicYear'];
+$Semester = $_POST['Semester'];
 
 
 try{
 
-    $existSQL = "SELECT * FROM tbl_section WHERE course = '$Course' AND section_year = '$Year' AND academic_year = '$AcademicYear'";
+    $existSQL = "SELECT * FROM tbl_section WHERE course = '$Course' AND section_year = '$Year' AND academic_year = '$AcademicYear' AND semester = '$Semester'";
     if($dataAdd = mysqli_query($con, $existSQL)){
         $rowcount = mysqli_num_rows($dataAdd);
         exit(json_encode(array("statusCode"=>$rowcount)));
