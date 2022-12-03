23ad2f7c4d1e06f73a6fff8257fc5312e5275093
Emmanuel <emmanuel.nocum@cvsu.edu.ph>
2022-12-03 10:32:28 +0800

    sdvsdvsdv

--- employee/backend/regular-schedule-table-registration.php
+++ employee/backend/regular-schedule-table-registration.php
@@ -9,10 +9,10 @@ $con = connection();
 $Year = $_['Year'];
 $AcademicYear = $_POST['AcademicYear'];
 $Semester = $_POST['Semester'];
-
+$Course = $_POST['Course'];
 try{
 
-    $sql = mysqli_query($con, "SELECT `tbl_subjectpersection`.* FROM `tbl_subjectpersection` WHERE `tbl_subjectpersection`.`sectionandsemester` = (SELECT `tbl_section`.`sectionandsemester` FROM `tbl_section` WHERE `tbl_section`.`academic_year` = '$AcademicYear' AND `tbl_section`.`section_year` ='$Year' AND `tbl_section`.`semester` ='$Semester'  AND `tbl_section`.`maxstudent` != `tbl_section`.`totalstudent` LIMIT 1)");
+    $sql = mysqli_query($con, "SELECT `tbl_subjectpersection`.* FROM `tbl_subjectpersection` WHERE `tbl_subjectpersection`.`sectionandsemester` = (SELECT `tbl_section`.`sectionandsemester` FROM `tbl_section` WHERE `tbl_section`.`academic_year` = '$AcademicYear' AND `tbl_section`.`section_year` ='$Year' AND `tbl_section`.`semester` ='$Semester' AND `tbl_section`.`course` = '$Course' AND `tbl_section`.`maxstudent` != `tbl_section`.`totalstudent` LIMIT 1)");
 
     //store in result
     
