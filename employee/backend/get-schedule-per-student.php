74eabe1b7182a8f60fc319d9a51aa93a70bdd562
Emmanuel <emmanuel.nocum@cvsu.edu.ph>
2022-12-04 16:06:44 +0800

    bdfbdfbdfb

--- employee/backend/get-schedule-per-student.php
+++ employee/backend/get-schedule-per-student.php
@@ -9,7 +9,7 @@ $con = connection();
 $StudentID = $_POST['StudentId'];
 $SectionAndSemester = $_POST['SectionAndSemester'];
 
-    $sql = mysqli_query($con,"SELECT * FROM `tbl_gradesperstudent`  WHERE `tbl_gradespertstudent`.`student_id` = '$StudentID' AND `tbl_gradesperstudent`.`sectionandsemester` = '$SectionAndSemester';");
+    $sql = mysqli_query($con,"SELECT * FROM `tbl_gradesperstudent`  WHERE `student_id` = '$StudentID' AND `sectionandsemester` = '$SectionAndSemester';");
 
     $result = mysqli_fetch_all($sql, MYSQLI_ASSOC);
   
