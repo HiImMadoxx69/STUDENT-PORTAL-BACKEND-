7c8877cfc11b1b5f60518deb00a250856847e421
Emmanuel <emmanuel.nocum@cvsu.edu.ph>
2022-12-05 17:59:07 +0800

    asfaf

--- employee/backend/fee-student-history.php
+++ employee/backend/fee-student-history.php
@@ -8,14 +8,25 @@ $StudentNumber = $_POST['StudentNumber'];
 
 try{
     
+
+
     $sql = mysqli_query($con, "SELECT 
     * FROM `tbl_updatehistory` WHERE `category` = 'StudentFee' AND `edited_email` = '$StudentNumber';");
     
     //store in result
     
     $result = mysqli_fetch_all($sql, MYSQLI_ASSOC);
+
+   
+$sqlstudent = mysqli_query($con, "SELECT * FROM `tbl_studentinfo` WHERE `tbl_studentinfo`.`studentnumber` = '$StudentNumber'  ORDER BY `id` DESC");
+
+$student = mysqli_fetch_all($sqlstudent, MYSQLI_ASSOC);
+
+$studentacc = mysqli_query($con,"SELECT * FROM `tbl_accountbalance` WHERE `tbl_accountbalance`.`studentnumber` = '$StudentID' ;");
+
+$balance = mysqli_fetch_all($studentacc, MYSQLI_ASSOC);
     
-    exit(json_encode($result));
+    exit(json_encode(array("history"=>$result, "student" => $student, "balance" => $balance )));
 }catch(Exception $e){
     exit(json_encode(array($e->getMessage())));
 }
