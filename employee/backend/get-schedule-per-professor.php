25d76865c3cf7d016e228fd9dd3f32f0ad317531
Emmanuel <emmanuel.nocum@cvsu.edu.ph>
2022-12-08 21:57:22 +0800

    asfasfas

--- employee/backend/get-schedule-per-professor.php
+++ employee/backend/get-schedule-per-professor.php
@@ -6,9 +6,9 @@
     include_once("../connections/connection.php");
 $con = connection();
 
-$Email = $_POST['Email'];
+$Username = $_POST['Username'];
 
-    $sql = mysqli_query($con,"SELECT  `tbl_subjectpersection`.* FROM `tbl_subjectpersection` LEFT JOIN `tbl_professor` ON  `tbl_subjectpersection`.`professor_initial` = `tbl_professor`.`professor_username` AND `tbl_professor`.`email` = '$Email';");
+    $sql = mysqli_query($con,"SELECT  `tbl_subjectpersection`.* FROM `tbl_subjectpersection`.`professor_initial` = '$Username';");
 
     $result = mysqli_fetch_all($sql, MYSQLI_ASSOC);
   
