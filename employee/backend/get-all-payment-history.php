174e449b32c0cef175d0774da986c6dd314aeae4
Emmanuel <emmanuel.nocum@cvsu.edu.ph>
2022-12-09 13:02:18 +0800

    sgsfg

--- employee/backend/get-all-payment-history.php
+++ employee/backend/get-all-payment-history.php
@@ -5,7 +5,7 @@ include_once("../connections/connection.php");
 $con = connection();
 
 
-$sql = mysqli_query($con, "SELECT * FROM `tbl_history`  ORDER BY `id` DESC");
+$sql = mysqli_query($con, "SELECT * FROM `tbl_history` WHERE `category` = 'StudentFee'  ORDER BY `id` DESC");
 
 //store in result
 
