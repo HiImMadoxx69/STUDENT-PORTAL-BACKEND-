1dc87634b0f19bb896640cb0d619f1cca31b7702
Emmanuel <emmanuel.nocum@cvsu.edu.ph>
2022-12-09 08:23:22 +0800

    dfbdfb

---
+++ employee/backend/check-password-correct-professor.php
@@ -0,0 +1,33 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Content-type: application/json');
+if(!isset($_SESSION)){
+  session_start();
+}
+
+include_once("../connections/connection.php");
+  
+$con = connection();
+try{
+
+
+
+if(isset($_POST['Email'])){
+    $email =  mysqli_real_escape_string($con, $_POST['Email']);
+    $password =   mysqli_real_escape_string($con,$_POST['Password']);
+
+    $checkLoginEmail = mysqli_query($con, "SELECT * FROM tbl_professor WHERE email = '$email'");
+    $rowData = mysqli_fetch_array($checkLoginEmail);
+    if (password_verify($password, $rowData['password'])) {
+        exit(json_encode(array("statusCode"=>200)));
+    }else{
+        exit(json_encode(array("statusCode"=>201)));
+    }
+}else{
+    exit(json_encode(array("statusCode"=>201)));
+}
+}catch(Exception $e){
+    echo json_encode(array("statusCode"=>$e->getMessage()));
+}
+  
+?>
